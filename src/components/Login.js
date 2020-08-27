e15fb307889b2c1773a80197260d2dcb206e5922
Sean Murakami <murakami.sean@yahoo.com>
2020-08-27 14:37:56 -0700

    Added styling to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,35 +6,62 @@ const LOGIN_URI = process.env.NODE_ENV !== "production" ? "http://localhost:8888
 
 const LoginContainer = styled.div`
   height: 100vh;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  background-image: -webkit-gradient(linear, left top, right top, from(#c074b2), to(#8ab5e8));
+  background-image: linear-gradient(90deg, #c074b2, #8ab5e8);
+`;
+
+const Gradient = styled.div`
+  width: 100%;
+  height: inherit;
+  position: absolute;
+  background-image: -webkit-gradient(linear, left top, left bottom, from(transparent), to(#000));
+  background-image: linear-gradient(transparent, #000);
+`;
+
+const Container = styled.div`
   display: flex;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
+  position: absolute;
+  height: inherit;
+  width: 100%;
 `;
 
 const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: ${({ theme: { colors } }) => colors.green};
+  font-size: 40px;
+  letter-spacing: 2px;
+  color: ${({ theme: { colors } }) => colors.white};
 `;
 
 const LoginBtn = styled.a`
-  background-color: #1db954;
-  color: white;
+  background-color: ${({ theme: { colors } }) => colors.green};
+  color: ${({ theme: { colors } }) => colors.white};
   border-radius: 30px;
   padding: 17px 35px;
+  margin-bottom: 100px;
   min-width: 160px;
   font-weight: 700;
   letter-spacing: 2px;
-  text-align: center;
+  transition: background-color 0.3s ease-in-out;
+  &:hover,
+  &:focus {
+    background-color: ${({ theme: { colors } }) => colors.lightGreen};
+  }
 `;
 
 export default () => {
   return (
     <Theme>
       <LoginContainer>
-        <Title>Please Log In</Title>
-        <LoginBtn href={LOGIN_URI}>Log In</LoginBtn>
+        <Gradient />
+        <Container>
+          <Title>Spotify Visuals</Title>
+          <LoginBtn href={LOGIN_URI}>LOGIN TO SPOTIFY</LoginBtn>
+        </Container>
       </LoginContainer>
     </Theme>
   );
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components";
 const theme = {
   colors: {
     green: "#1DB954",
+    lightGreen: "#1bd85f",
     black: "#191414",
     white: "#FFFFFF",
   },
