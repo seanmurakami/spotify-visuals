598f063ac0ffaf5d58ab38883e59e52eaeb1514e
Sean Murakami <murakami.sean@yahoo.com>
2020-08-26 23:35:02 -0700

    Adjusted styling of login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Theme from "../styles/Theme";
 
 const LOGIN_URI = process.env.NODE_ENV !== "production" ? "http://localhost:8888/login" : "https://spotify-visuals.herokuapp.com/login";
 
@@ -14,14 +15,27 @@ const LoginContainer = styled.div`
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
+  color: ${({ theme: { colors } }) => colors.green};
+`;
+
+const LoginBtn = styled.a`
+  background-color: #1db954;
+  color: white;
+  border-radius: 30px;
+  padding: 17px 35px;
+  min-width: 160px;
+  font-weight: 700;
+  letter-spacing: 2px;
+  text-align: center;
 `;
 
 export default () => {
   return (
-    <LoginContainer>
-      <Title>Please Sign In</Title>
-      <a href={LOGIN_URI}>Login</a>
-    </LoginContainer>
+    <Theme>
+      <LoginContainer>
+        <Title>Please Log In</Title>
+        <LoginBtn href={LOGIN_URI}>Log In</LoginBtn>
+      </LoginContainer>
+    </Theme>
   );
 };
