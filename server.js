5e3698bf4034b8f4b833040c04dc233f04031fbc
Sean Murakami <murakami.sean@yahoo.com>
2020-09-03 16:17:09 -0700

    Update scope to account for user top tracks

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/login", function (req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = "user-read-private user-read-email user-follow-read playlist-read-private";
+  var scope = "user-read-private user-read-email user-follow-read playlist-read-private user-top-read";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
