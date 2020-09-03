301f029d1c981f2fd46d685b83ebc335ef66e20e
Sean Murakami <murakami.sean@yahoo.com>
2020-09-03 10:58:45 -0700

    update spotify api scope and get artists the user follows

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/login", function (req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = "user-read-private user-read-email";
+  var scope = "user-read-private user-read-email user-follow-read playlist-read-private";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
